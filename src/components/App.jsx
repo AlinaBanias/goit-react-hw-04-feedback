ef12136be43d549b7223465db2aff11afd4afb8a
AlinaBanias <isabella08092001@gmail.com>
2023-04-17 16:47:52 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,70 @@
-import FeedbackForm from './FeedbackForm/';
+import { useState } from 'react';
+import Statistics from './Statistics';
+import FeedbackOptions from './FeedbackOptions';
+import Section from './Section';
 
 export const App = () => {
-  return <FeedbackForm></FeedbackForm>;
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  
+
+  const options = ['good', 'neutral', 'bad'];
+
+  const handleFeedbackBtn = e => {
+    const keyName = e.target.name;
+
+    switch (keyName) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1);
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const totalFeedback = () => {
+    console.log(good + neutral + bad);
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const totalFeedback = countTotalFeedback();
+
+    const percentage = Math.round((good / totalFeedback) * 100);
+
+    return percentage ? percentage : 0;
+  };
+
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={options}
+          onLeaveFeedback={handleFeedbackBtn}
+        />
+      </Section>
+
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedback}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        />
+      </Section>
+     </>
+  );
 };
+
+
